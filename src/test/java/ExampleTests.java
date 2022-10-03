13f4d2e0c58367f8e65e69e11372f90f800b398e
Anna Tezelashvili <tezelashviliann@gmail.com>
2022-10-03 18:50:21 +0400

    first task

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,9 +18,18 @@ public class ExampleTests {
         WebElement button=driver.findElement(By.xpath("//ul/li/a"));
         button.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
+        Assert.assertNotEquals(text.getText(),"No Test");
+        // 1. Navigate back to the main page
+        driver.navigate().back();
+        // 2. Click on DropDown
+        WebElement dropdown= driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[11]/a"));
+        dropdown.click();
+        // 3. Select 'Option 1'
+        WebElement dropdownElement =  driver.findElement(By.id("dropdown"));
+        Select dropdownSelect = new Select(dropdownElement);
+        dropdownSelect.selectByVisibleText("Option 1");
 
-
+        driver.close();
 
     }
 }
