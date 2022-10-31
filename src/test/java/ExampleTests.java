d378be8c83e1ed6be0b40cf9dfa6c99fefcd2323
Anna Tezelashvili <tezelashviliann@gmail.com>
2022-10-31 12:31:56 +0400

    update examples file

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ExampleTests {
 
     @Test
@@ -31,5 +33,33 @@ public class ExampleTests {
 
         driver.close();
 
+    }
+    @Test
+    public void locatorTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://tutorialsninja.com/demo/index.php?route=product/category&path=33");
+        driver.manage().window().maximize();
+        //*[@id="content"]/div/div[2]/h1 - XPath
+        //#content > div > div.col-sm-4 > h1 - css
+        List<WebElement> anchors = driver.findElements(By.className("product-thumb"));
+        Assert.assertEquals(anchors.size(),2);
+        WebElement button=driver.findElement(By.partialLinkText("Canon EOS 5D"));
+        button.click();
+    }
+    @Test
+    public void browserCommands(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://tutorialsninja.com/demo/index.php?route=product/category&path=33");
+        // Get the title of the page
+        String title = driver.getTitle();
+        System.out.println("Title " + title);
+        // Get the current URL
+        String url = driver.getCurrentUrl();
+        System.out.println("URL " + url);
+        // Get the current page HTML source
+        String html = driver.getPageSource();
+        System.out.println("HTML " + html);
     }
 }
